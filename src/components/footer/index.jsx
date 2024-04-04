275a9eda0d4de9d0da1b11234a21172fe964bd0f
hritikcps <hritik.raj@cloudprism.in>
2024-04-04 18:46:25 +0530

    minor changes

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
     return (
         <div className='flex justify-center items-center mt-[8rem]'>
             <div className='grid grid-cols-2 gap-4 text-white'>
-                <div className='col-span-1 row-span-3 flex flex-col items-center text-right'>
+                <div className='col-span-1 row-span-3 flex flex-col items-center  mr-[5rem]'>
                     <img src={logo} alt="logo" className='mb-[2rem]' />
-                    <p className='text-xl mb-[1.5rem]'>Contact Us</p>
-                    <p style={{ maxWidth: '300px' }}>Sandesh sadan near mamta apartment, south shivpuri Patna 800023</p>
+                    <p className='text-xl mb-[1.5rem] mr-[8rem]'>Contact Us</p>
+                    <p style={{ maxWidth: '280px' }}>Sandesh sadan near mamta apartment, south shivpuri Patna 800023</p>
                 </div>
 
-                <div className='flex items-center flex-col flex-wrap justify-between mt-[4rem] text-right '>
+                <div className='flex items-center flex-col flex-wrap justify-between mt-[4rem] ml-[5rem] text-right '>
                     <img src={twitter} alt="twitter logo" className=' w-10 h-10 mb-[1.3rem]' />
                     <p>+917870081711</p>
                     <p>join@codingcommando.in</p>
